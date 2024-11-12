bdc711e52f8107b98326682431e3debf900d1d65
Emil Krutikov <lonelyflow123@gmail.com>
2024-11-12 19:56:10 +0500

    fix: fix error while get schedule

--- src/main/java/me/krutikov/schedule/service/UunitScheduleService.java
+++ src/main/java/me/krutikov/schedule/service/UunitScheduleService.java
@@ -5,17 +5,23 @@ import me.krutikov.schedule.data.ScheduleDay;
 import me.krutikov.schedule.data.ScheduleWeek;
 import me.krutikov.schedule.parser.ScheduleParser;
 import me.krutikov.schedule.parser.UunitScheduleParser;
-import me.krutikov.schedule.utils.RequestUtils;
 import org.jsoup.Jsoup;
 
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 public class UunitScheduleService implements ScheduleService {
 
-    private static final String GROUP_ID_URL = "http://edu.strbsu.ru/php/getIdGroup.php";
-    private static final String SCHEDULE_URL = "http://edu.strbsu.ru/php/getShedule.php";
+    private static final String GROUP_ID_URL = "http://edu.struust.ru/php/getIdGroup.php";
+    private static final String SCHEDULE_URL = "http://edu.struust.ru/php/getShedule.php";
 
     private final ScheduleParser scheduleParser = new UunitScheduleParser();
+    private final HttpClient httpClient = HttpClient.newHttpClient();
 
     @Override
     public ScheduleWeek getSchedule(final String group) {
@@ -24,9 +30,17 @@ public class UunitScheduleService implements ScheduleService {
 
     @Override
     public ScheduleWeek getSchedule(final String group, final int week) {
+        val request = HttpRequest.newBuilder()
+                .uri(URI.create(SCHEDULE_URL))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString("type=2&id=%s&week=%s".formatted(
+                        getGroupId(group), week
+                )))
+                .build();
+
         String rawSchedule;
         try {
-            rawSchedule = RequestUtils.postRequest(SCHEDULE_URL, String.format("type=2&id=%s&week=%s", getGroupId(group), week));
+            rawSchedule = httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -54,10 +68,17 @@ public class UunitScheduleService implements ScheduleService {
 
     @Override
     public int getGroupId(final String group) {
+        String params = "group_name=" + URLEncoder.encode(group, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(GROUP_ID_URL))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(params))
+                .build();
         try {
-            return Integer.parseInt(RequestUtils.postRequest(GROUP_ID_URL, "group_name=" + group));
+            val response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            return Integer.parseInt(response.body());
         } catch (Exception e) {
-            e.printStackTrace();
             return 0;
         }
     }
